af61222cee7a6b6d3318850239274ba4a9d10a24
Xheir <emeza2019@inf.udec.cl>
2022-09-08 20:52:20 -0400

    Nuevas Funciones
    Falta check_metrica que permita aplicar la metrica

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import utility_functions as uf
 
 def main():
-    # sequence=["ACGTA","CCCCG","GTACT","GTCCC","GTGGA","GTGGG"]
-    file_number=input("Enter the number of the file you want to open: ")
+    # file_number=input("Enter the number of the file you want to open: ")
+    # threshold=float(input("Enter the threshold value"))
+    file_number=69
+    threshold=0.75
     sequence=uf.get_Input_From_File(int(file_number))
-    uf.Greedy(sequence)
+    uf.Greedy(sequence,threshold)
 
 
 if __name__ == '__main__':
--- utility_functions.py
+++ utility_functions.py
@@ -1,5 +1,8 @@
 import os
 from fileinput import filename
+from select import select
+from turtle import position
+import numpy as np
 import random
 
 def get_Input_From_File(number):
@@ -40,26 +43,16 @@ def local_Search():
             no_Local=False
     return False
 
-def GRASP(number_of_iterations):
-    i=1
-    for i in range (number_of_iterations):
-        #build a greedy radoomized solution
-        x= local_Search()
-        if(i==1):
-            best_x=x
-        else:
-            if(x<best_x):
-                best_x=x
-    return best_x
+
 
 def less_freq_in_every_column(sequences,row,column):
-    print("here")
-    less_freq=[]
+    answer=[]
     for i in range(column-1):
         counter_A=0
         counter_C=0
         counter_G=0
         counter_T=0
+        selective_charachter=[]
         for j in range(row):
             if(sequences[j][i]=="A"):
                 counter_A=counter_A+1
@@ -69,43 +62,65 @@ def less_freq_in_every_column(sequences,row,column):
                 counter_G=counter_G+1
             elif(sequences[j][i]=="T"):
                 counter_T=counter_T+1
-        less_freq.append("column nr "+str(i)+": ")
-        if(counter_A<=min(counter_A,counter_C,counter_G,counter_T)):
-            less_freq.append("A")
-        if(counter_C<=min(counter_A,counter_C,counter_G,counter_T)):
-            less_freq.append("C")
-        if(counter_G<=min(counter_A,counter_C,counter_G,counter_T)):
-            less_freq.append("G")
-        if(counter_T<=min(counter_A,counter_C,counter_G,counter_T)):
-            less_freq.append("T")
-        less_freq.append("|")
-    return less_freq
+        if(counter_A==min(counter_A,counter_C,counter_G,counter_T)):
+            selective_charachter.append("A")
+        if(counter_C==min(counter_A,counter_C,counter_G,counter_T)):
+            selective_charachter.append("C")
+        if(counter_G==min(counter_A,counter_C,counter_G,counter_T)):
+            selective_charachter.append("G")
+        if(counter_T==min(counter_A,counter_C,counter_G,counter_T)):
+            selective_charachter.append("T")
+        answer.append(selective_charachter)
+    return answer
 
 
-def Greedy(sequence):
-    threshold=createThreshold()
-    #create a matrix of n sequences of m
+def Greedy(sequence,threshold):
     n=len(sequence)
     m=len(sequence[0])
-    matrix=[]
-    for i in range(n):
-        matrix.append([])
-        for j in range(m):
-            matrix[i].append(sequence[i][j])
-    #print the whole matrix
-    for i in range(n):
-        for j in range(m):
-            print(matrix[i][j], end=" ")
-        print()
-    #get the less frequent letter in each column
     less_freq=[]
     less_freq=less_freq_in_every_column(sequence,n, m)
-    #print the less frequent letter in each column in the column their belong
-    for i in range(len(less_freq)):
-        print(less_freq[i], end=" ")
-    print()
+    contrucSolution(sequence,n,less_freq,threshold)
 
-def createThreshold():
-    #return a random float number between 0 and 1, with 3 decimals
-    threshold=round(random.uniform(0, 1), 3)
-    return threshold
+def min_Hamming_Distance(len_sequences,threshold):
+    value=int(threshold*len_sequences)
+    return value
+
+def contrucSolution(sequence,len_sequences,less_freq,threshold):
+    metric=min_Hamming_Distance(len_sequences,threshold)
+    answer=[]
+    for i in range(len(sequence[0])):
+        if(len(answer)<=metric):
+            answer.append(check_less_freq(less_freq,i)) 
+        else:
+            answer.append(check_Metric(answer,sequence,metric))            
+    print(answer)
+        
+    
+def check_less_freq(less_freq,i):
+    characters=less_freq[i]
+    if(len(characters)==1):
+        return characters[0]
+    else:
+        return random.choice(characters)
+
+def check_Metric(answer,sequence,metric):
+    copy_answer=answer
+    value_A=check_sequence(sequence,copy_answer,metric,"A")
+    #remove the last element of the list copy_answer
+    copy_answer.pop()
+    value_C=check_sequence(sequence,copy_answer,metric,"C")
+    copy_answer.pop()
+    value_G=check_sequence(sequence,copy_answer,metric,"G")
+    copy_answer.pop()
+    value_T=check_sequence(sequence,copy_answer,metric,"T")
+    #print(value_A,value_C,value_G,value_T)
+    return True
+
+def check_sequence(sequence,answer,metric,character):
+    answer.append(character)
+    counter=0
+    for i in range(len(answer)):
+        hammin_distance=get_Hamming_Distance(answer,sequence[i])
+        if(hammin_distance>=metric):
+            counter=counter+1
+    return counter
