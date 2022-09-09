fdf2cce254c36d0c1006161e5eea9c372aac6072
Xheir <emeza2019@inf.udec.cl>
2022-09-09 19:44:10 -0400

    Timer + Data
    falta ver los ifs iniciales para evitar prints innecesarios

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+from fileinput import filename
 import sys
 import Services.FileManager as fm
+import Services.Timer as tm
 import Functions.Greedys as gd
 
 def main():
-    file_name=sys.argv[2]
-    threshold=sys.argv[4]
-    sequence=fm.open_File_By_Name(file_name)
-    gd.Greedy(sequence,threshold)
-    
+    if(sys.argv[1]=="-t"):
+        tm.get_answer_Time()
+    else:
+        file_name=sys.argv[2]
+        threshold=sys.argv[4]
+        sequence=fm.open_File_By_Name(file_name)
+        gd.Greedy(sequence,threshold)
 
 if __name__ == '__main__':
     if(len(sys.argv)==1):
@@ -18,6 +22,10 @@ if __name__ == '__main__':
         print("Usage: python3 main.py -i [filename] -th [number]")
         print("Example: python3 main.py -i sequences.txt -th 0.5")
         print("Because of problems nature, threshold must be a number between 0 and 1")
+
+    if(len(sys.argv)==2 and sys.argv[1]=="-t"):
+        main()
+
     if(len(sys.argv)==5 and sys.argv[1]=="-i" and sys.argv[3]=="-th"):
         print("Correct execution")
         main()
