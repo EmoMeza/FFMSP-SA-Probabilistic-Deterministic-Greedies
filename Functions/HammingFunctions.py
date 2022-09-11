eda0cfa3b3129e829e40b91c7c4847b7de53c79d
Xheir <69222732+EmoMeza@users.noreply.github.com>
2022-09-10 20:21:34 -0400

    Almost Working
    There's an error in the code, UtilityFunctions.py line 37.

--- Functions/HammingFunctions.py
+++ Functions/HammingFunctions.py
@@ -1,6 +1,11 @@
-def get_Hamming_Distance(sequence1, sequence2):
+def get_Hamming_Distance(sequence1, sequence2, metric):
     cont=0
     for i in range(len(sequence1)):
+        if(sequence2.get_metric_Satisfied()==True):
+            return metric
+        if(cont>=metric):
+            sequence2.change_metric_Satisfied(True)
+            return metric
         if sequence1[i]!=sequence2.get_Character(i):
             cont=cont+1
     return cont
